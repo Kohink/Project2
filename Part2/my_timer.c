299f314aaa43c9966d7ed2906135a4417f4456f0
Kohin <kohin1999@hotmail.com>
2021-03-21 14:18:09 -0400

    my_timer.c

--- Part2/my_timer.c
+++ Part2/my_timer.c
@@ -4,38 +4,33 @@
 #include <linux/string.h>
 #include <linux/proc_fs.h>
 #include <linux/uaccess.h>
-#include <time.h>
+#include <linux/time.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 #define BUF_LEN 100//max length of read/write message
-static structproc_dir_entry* proc_entry;//pointer to procentry
+static struct proc_dir_entry* proc_entry;//pointer to procentry
 static char msg[BUF_LEN];//buffer to store read/write message
-static intprocfs_buf_len;//variable to hold length of message
-
-static structfile_operationsprocfile_fops= 
-{
-    .owner = THIS_MODULE,
-    .read = procfile_read, 
-    .write = procfile_write,
-};
+static int procfs_buf_len;//variable to hold length of message
 
 // read
-static ssize_tprocfile_read(structfile* file, char * ubuf, size_tcount, loff_t*ppos)
+static ssize_t procfile_read(struct file* file, char * ubuf, size_t count, loff_t *ppos)
 {
-    int current_time = current_kernal_time();
-    int elapsed_time = 0;
+    struct timespec current_time;
+    current_time = current_kernel_time();
+    long int elapsed_time = ts.tv_sec;
+    long int elapsed_time2 = ts.tv_nsec;
     int counter = 0;
 
     printk(KERN_INFO "proc_read\n");
-    procfs_buf_len= strlen(msg);
+    procfs_buf_len = strlen(msg);
     if (*ppos> 0 || count < procfs_buf_len) //check if data already read and if space in user buffer
         return 0;
     
-    printf("current time: %d", current_time);
+    sprintf("current time: %ld", current_time);
     if(counter > 0)
     {
-        printf("elapsed time: %d", elapsed_time);
-        elapsed_time += elasped_time;
+        sprintf("elapsed time: %ld", elapsed_time);
+        elapsed_time += elasped_time2;
     }
     counter++;
 
@@ -47,29 +42,39 @@ static ssize_tprocfile_read(structfile* file, char * ubuf, size_tcount, loff_t*p
     return procfs_buf_len; //return number of characters read
 
 // write
-static ssize_tprocfile_write(structfile* file, constchar * ubuf, size_tcount, loff_t* ppos)
+static ssize_t procfile_write(struct file* file, const char * ubuf, size_t count, loff_t *ppos)
 {
     printk(KERN_INFO "proc_write\n");
     if (count > BUF_LEN)
-        procfs_buf_len= BUF_LEN;
+        procfs_buf_len = BUF_LEN;
     else
-        procfs_buf_len= count;
+        procfs_buf_len = count;
     copy_from_user(msg, ubuf, procfs_buf_len);
     printk(KERN_INFO "got from user: %s\n", msg);
     return procfs_buf_len;
 }
 
-// init
-static inthello_init(void)
+static struct file_operations procfile_fops= 
 {
-    proc_entry= proc_create("hello", 0666, NULL, &procfile_fops);
-    if (proc_entry== NULL)
+    .owner = THIS_MODULE,
+    .read = procfile_read, 
+    .write = procfile_write,
+};
+
+// init
+static int my_timer_init(void)
+{
+    proc_entry= proc_create("my_timer", 0666, NULL, &procfile_fops);
+    if (proc_entry == NULL)
         return -ENOMEM;
 return 0;
 
 // exit
-static void hello_exit(void)
+static void my_timer_exit(void)
 {
     proc_remove(proc_entry);
     return;
 }
+
+module_init(my_timer_init);
+module_exit(my_timer_exit);
