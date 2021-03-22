981bc599540d4ec1dff58a352b82f10504439c9a
Carson Walker <66083888+CWalker561@users.noreply.github.com>
2021-03-21 20:10:48 -0400

    part 2

--- Part2/my_timer.c
+++ Part2/my_timer.c
@@ -7,32 +7,57 @@
 #include <linux/time.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
-#define BUF_LEN 100//max length of read/write message
+#define BUF_LEN 500//max length of read/write message
 static struct proc_dir_entry* proc_entry;//pointer to procentry
 static char msg[BUF_LEN];//buffer to store read/write message
 static int procfs_buf_len;//variable to hold length of message
+static long int oldSec, oldNano;
+static bool hasPrevious = false;
+const int secToNano = 999999999;
 
 // read
 static ssize_t procfile_read(struct file* file, char * ubuf, size_t count, loff_t *ppos)
 {
     struct timespec current_time;
     current_time = current_kernel_time();
-    long int elapsed_time = ts.tv_sec;
-    long int elapsed_time2 = ts.tv_nsec;
-    int counter = 0;
+
+    long int seconds, nano;
+    long int secDif, nanoDif;
+
+    seconds = current_time.tv_sec;
+    nano = current_time.tv_nsec;
 
     printk(KERN_INFO "proc_read\n");
     procfs_buf_len = strlen(msg);
     if (*ppos> 0 || count < procfs_buf_len) //check if data already read and if space in user buffer
         return 0;
-    
-    sprintf("current time: %ld", current_time);
-    if(counter > 0)
+
+    if (hasPrevious == true)
     {
-        sprintf("elapsed time: %ld", elapsed_time);
-        elapsed_time += elasped_time2;
+	long int checkForNegNano = nano - oldNano;
+	
+	if (checkForNegNano < 0)
+    	{
+    	    secDif = (seconds - oldSec) - 1;
+	    nanoDif = secToNano + checkForNegNano;
+    	}
+	else
+	{
+	    secDif = seconds - oldSec;
+	    nanoDif = checkForNegNano;
+	}
+
+        procfs_buf_len += sprintf(msg,"current time: %ld.%ld\nelapsed time: %ld.%09ld\n\n", seconds, nano, secDif, nanoDif);
     }
-    counter++;
+    else
+    {
+        procfs_buf_len += sprintf(msg, "current time: %ld.%ld\n\n", seconds, nano);
+	hasPrevious = true;
+    }
+
+    oldSec = seconds;
+    oldNano = nano;
+    
 
     if (copy_to_user(ubuf, msg, procfs_buf_len)) //send data to user buffer
         return -EFAULT;
@@ -40,6 +65,7 @@ static ssize_t procfile_read(struct file* file, char * ubuf, size_t count, loff_
     printk(KERN_INFO "gave to user %s\n", msg);
 
     return procfs_buf_len; //return number of characters read
+}
 
 // write
 static ssize_t procfile_write(struct file* file, const char * ubuf, size_t count, loff_t *ppos)
@@ -64,10 +90,11 @@ static struct file_operations procfile_fops=
 // init
 static int my_timer_init(void)
 {
-    proc_entry= proc_create("my_timer", 0666, NULL, &procfile_fops);
+    proc_entry= proc_create("timer", 0666, NULL, &procfile_fops);
     if (proc_entry == NULL)
         return -ENOMEM;
-return 0;
+    return 0;
+}
 
 // exit
 static void my_timer_exit(void)
